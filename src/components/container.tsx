c01bc26c785ef11833ef3c99f4f39aa219f0d836
Dani Kellogg <dani@productcolab.com>
2025-10-05 15:19:39 -0700

    Fix Container component to accept id prop for navigation anchors

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,12 +3,14 @@ import { clsx } from 'clsx'
 export function Container({
   className,
   children,
+  id,
 }: {
   className?: string
   children: React.ReactNode
+  id?: string
 }) {
   return (
-    <div className={clsx(className, 'px-6 lg:px-8')}>
+    <div id={id} className={clsx(className, 'px-6 lg:px-8')}>
       <div className="mx-auto max-w-2xl lg:max-w-7xl">{children}</div>
     </div>
   )
